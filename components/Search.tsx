8585c799dfe81b0896cb4c4e20f7a704fadc5fed
isamarietr <isa.torres@gmail.com>
2021-05-05 13:49:48 -0400

    Flatten object display

--- components/Search.tsx
+++ components/Search.tsx
@@ -35,7 +35,7 @@ const Search = ({ indexFields, actions, state }: Props) => {
   const renderPagination = () => {
     let pagesEl = null
 
-    if(numPages > 1){
+    if (numPages > 1) {
       let active = currPage > 0 ? currPage : 1;
       let items = [];
       const maxPages = numPages < 25 ? numPages : 25;
@@ -46,16 +46,29 @@ const Search = ({ indexFields, actions, state }: Props) => {
           </Pagination.Item>,
         );
       }
-      if(numPages > maxPages){
+      if (numPages > maxPages) {
         items.push(
           <Pagination.Ellipsis />
         );
       }
       pagesEl = <Pagination>{items}</Pagination>
     }
-    
+
     return pagesEl
   }
+
+  // recursive fn
+  const renderResultObject = (key, value, level, parent?) => {
+    const keyPath = parent ? `${parent}.${key}` : key
+    if (typeof value === 'object' && !Array.isArray(value)) {
+      return Object.keys(value).map((key, index) => {
+        return renderResultObject(key, value[key], level + 1, keyPath)
+      })
+    }
+
+    return <Row key={`card-body`} > <code className={value.toString().toUpperCase().includes(query.toUpperCase()) ? 'highlight' : ''}><b>{keyPath}</b>: {Array.isArray(value) ? value.join(', ') : value}</code></Row>
+  }
+
   /**
    * renderResults
    * @returns 
@@ -73,10 +86,9 @@ const Search = ({ indexFields, actions, state }: Props) => {
               <Card.Body>
                 {
                   Object.keys(result).map((key, index) => {
-                    const value = JSON.stringify(result[key], null, 2) 
-                    return <Row key={`card-body-${index}`} ><code className={value.toUpperCase().includes(query.toUpperCase()) ? 'highlight' : ''}><b>{key}</b>: { value }</code></Row>
+                    return renderResultObject(key, result[key], 0)
                   })
-              }
+                }
               </Card.Body>
             </Accordion.Collapse>
           </Card>
@@ -86,9 +98,9 @@ const Search = ({ indexFields, actions, state }: Props) => {
     return (
       <Accordion className="mt-5 mb-5" defaultActiveKey="0">
         {results ? <p>Found {resultsCount} results</p> : null}
-        {results ? <Row sm={6}  className="mx-0">
+        {results ? <Row sm={6} className="mx-0">
           {renderPagination()}
-          </Row> : null}
+        </Row> : null}
         {resultsEl}
       </Accordion>)
   }
@@ -99,14 +111,14 @@ const Search = ({ indexFields, actions, state }: Props) => {
    */
   const onSubmit = async (event: any, page?: number) => {
 
-    if(!page){
+    if (!page) {
       setCurrPage(1)
     }
-    axios.get(`/api/search?query=${query}&path=${searchPath}&page=${page? page : 1}&limit=${searchLimit}&fuzzy=${isFuzzyMatch}`).then(response => {
+    axios.get(`/api/search?query=${query}&path=${searchPath}&page=${page ? page : 1}&limit=${searchLimit}&fuzzy=${isFuzzyMatch}`).then(response => {
       console.log(`data`, response);
       setResults(response.data.result);
       setResultsCount(response.data.total);
-      setNumPages(Math.ceil(response.data.total/searchLimit));
+      setNumPages(Math.ceil(response.data.total / searchLimit));
       // actions.setResults(response.data);
     }).catch(error => {
       console.log(error.response)
@@ -135,22 +147,22 @@ const Search = ({ indexFields, actions, state }: Props) => {
             <h1 className="title">Atlas Search <span className="subtitle">{state.title}</span> </h1>
           </Row>
           <Form className="mt-4">
-          <Form.Text className="mb-1" muted>
-                  Searching for values in { searchPath === '*' ? `all fields` : indexFields.join(', ')}
-                </Form.Text>
+            <Form.Text className="mb-1" muted>
+              Searching for values in {searchPath === '*' ? `all fields` : indexFields.join(', ')}
+            </Form.Text>
             <Form.Row className="align-items-center">
               <Col sm={4} className="my-1">
-                <Form.Control placeholder={`Enter your search text...`} onChange={onQueryChange} onKeyDown={(event) => { if (query?.length && event.key === 'Enter') { onSubmit(event) }}} value={query} />
+                <Form.Control placeholder={`Enter your search text...`} onChange={onQueryChange} onKeyDown={(event) => { if (query?.length && event.key === 'Enter') { onSubmit(event) } }} value={query} />
               </Col>
               <Col sm={4} className="my-1">
-            <Button type="submit" className="my-1" onClick={onSubmit} disabled={!query || !query.length ? true : false}>
-                Search
+                <Button type="submit" className="my-1" onClick={onSubmit} disabled={!query || !query.length ? true : false}>
+                  Search
               </Button>
               </Col>
-             
+
             </Form.Row>
             <Form.Row className="align-items-center">
-            <Col xs="auto" className="my-1" >
+              <Col xs="auto" className="my-1" >
                 <Form.Check
                   type="switch"
                   id="custom-switch"
@@ -159,7 +171,7 @@ const Search = ({ indexFields, actions, state }: Props) => {
                     setFuzzyMatch(!isFuzzyMatch)
                   }}
                 />
-                
+
               </Col>
               {/* <Col xs="auto" className="my-1" >
               <Form.Check
@@ -178,7 +190,7 @@ const Search = ({ indexFields, actions, state }: Props) => {
             </Col>
           </Row>
 
-          
+
         </Col>
 
       </Container>
