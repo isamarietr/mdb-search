31541fabd09dc5eff665c5cbc4bf98ac14658a2b
isamarietr <isa.torres@gmail.com>
2021-07-26 08:12:45 -0400

    Address anonymous function warning

--- redux/reducers/defaultReducer.ts
+++ redux/reducers/defaultReducer.ts
@@ -1,7 +1,7 @@
 import { initialState } from "../initial-state";
 import * as ACTIONS from "../actionTypes";
 
-export default function(state = initialState, action) {
+export default function DefaultReducer(state = initialState, action) {
   switch (action.type) {
     case ACTIONS.SET_LOADING: {
       const { isLoading } = action.payload;
